d92762b02897fc57c906de3bcd9df8b24392b711
chinnur <79906071+chinnur@users.noreply.github.com>
2021-05-07 17:01:24 +0530

    Delete WirelessTransmission directory

--- WirelessTransmission/includes.php
+++
@@ -1,6 +0,0 @@
-<link  href="<?php getSimCss('simstyle.css'); ?>" rel="stylesheet" type="text/css" />
-<script type="text/javascript" language="javascript" src="<?php getSimPath(); ?>js/jquery-ui-1.8.21.custom.min.js"></script>
-
-<script type="text/javascript" language="javascript" src="<?php getSimPath(); ?>js/jquery.qtip-1.0.0-rc3.min.js"></script>
-<!--Include files here to call them in head section-->
-
