0cd1966fb7e75d2473ac0fd19c172de26a0a3a5f
chinnur <79906071+chinnur@users.noreply.github.com>
2021-05-07 17:01:10 +0530

    Delete WirelessSensorNetwork directory

--- WirelessSensorNetwork/includes.php
+++
@@ -1,5 +0,0 @@
-<link  href="<?php getSimCss('simstyle.css'); ?>" rel="stylesheet" type="text/css" />
-<!--Include files here to call them in head section-->
-<script type="text/javascript" language="javascript" src="<?php getSimPath(); ?>js/jquery.qtip-1.0.0-rc3.min.js"></script>
-<!--<script type="text/javascript"language="javascript" src="<?php getSimPath(); ?>js/jquery.balloon.js"></script>-->
-
